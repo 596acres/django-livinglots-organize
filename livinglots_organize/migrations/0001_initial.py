4a6f76dfb5a0a1e357c9343cc00f9f4d20fff7af
Eric Brelsford <ebrelsford@gmail.com>
2014-11-17 16:04:56 -0500

    New style migrations

--- livinglots_organize/migrations/0001_initial.py
+++ livinglots_organize/migrations/0001_initial.py
@@ -1,34 +1,24 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'OrganizerType'
-        db.create_table(u'livinglots_organize_organizertype', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=64)),
-            ('is_group', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal(u'livinglots_organize', ['OrganizerType'])
+    dependencies = [
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'OrganizerType'
-        db.delete_table(u'livinglots_organize_organizertype')
-
-
-    models = {
-        u'livinglots_organize.organizertype': {
-            'Meta': {'object_name': 'OrganizerType'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_group': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
-        }
-    }
-
-    complete_apps = ['livinglots_organize']
+    operations = [
+        migrations.CreateModel(
+            name='OrganizerType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=64)),
+                ('is_group', models.BooleanField(default=False)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
